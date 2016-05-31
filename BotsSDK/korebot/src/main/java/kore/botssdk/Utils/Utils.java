a8ff5a6e1ca781043669d6562aeeaaf5ecac851e
Pradeep Mahato <pradeep.mahato@kore.com>
2016-05-31 18:53:26 +0530

    Added Appcontrol and DimentionUtils to hold the value of dp1 etc...

--- BotsSDK/korebot/src/main/java/kore/botssdk/Utils/Utils.java
+++ BotsSDK/korebot/src/main/java/kore/botssdk/Utils/Utils.java
@@ -2,6 +2,7 @@ package kore.botssdk.utils;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.util.DisplayMetrics;
 
 /**
  * Created by Pradeep Mahato on 30-May-16.
@@ -20,6 +21,16 @@ public class Utils {
     public static String getBuildVersion(Context context) {
         //return "v " + getServerInfo(context) + "_" + getVersion(context);
         return "v" + getVersion(context);
+    }
+
+    public static float convertDpToPixel(Context context, float dp) {
+
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        if(metrics.densityDpi<=DisplayMetrics.DENSITY_HIGH){
+            dp=1.4f;
+        }
+        float px = dp * (metrics.densityDpi / 160f);
+        return px;
     }
 
 }
---
+++ BotsSDK/korebot/src/main/java/kore/botssdk/application/AppControl.java
@@ -0,0 +1,30 @@
+package kore.botssdk.application;
+
+import android.content.Context;
+
+import kore.botssdk.view.DimensionUtil;
+
+/**
+ * Created by Pradeep Mahato on 31-May-16.
+ */
+public class AppControl {
+
+    Context context;
+    static AppControl singleton;
+    DimensionUtil dimensionUtil;
+
+    public AppControl(Context context) {
+        this.context = context;
+        singleton = this;
+
+        dimensionUtil = new DimensionUtil(context);
+    }
+
+    public static AppControl getInstance() {
+        return singleton;
+    }
+
+    public DimensionUtil getDimensionUtil() {
+        return dimensionUtil;
+    }
+}
