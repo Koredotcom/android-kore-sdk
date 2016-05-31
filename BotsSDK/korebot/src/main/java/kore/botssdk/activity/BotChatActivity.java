c89f20c4d21cae08b37ced707acd74b2f55d0e8c
Pradeep Mahato <pradeep.mahato@kore.com>
2016-05-31 18:41:26 +0530

    Changed BotChatActivity to hold 2 fragments : Content and Footer

--- BotsSDK/korebot/src/main/java/kore/botssdk/activity/BotChatActivity.java
+++ BotsSDK/korebot/src/main/java/kore/botssdk/activity/BotChatActivity.java
@@ -1,17 +1,44 @@
 package kore.botssdk.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.FrameLayout;
 
 import kore.botssdk.R;
+import kore.botssdk.fragment.BotContentFragment;
+import kore.botssdk.fragment.ComposeFooterFragment;
 
 /**
  * Created by Pradeep Mahato on 31-May-16.
  */
 public class BotChatActivity extends BaseSpiceActivity {
 
+    FrameLayout chatLayoutFooterContainer;
+    FrameLayout chatLayoutContentContainer;
+
+    FragmentTransaction fragmentTransaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bot_chat_layout);
+        findViews();
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        //Add Bot Content Fragment
+        BotContentFragment botContentFragment = new BotContentFragment();
+        fragmentTransaction.add(R.id.chatLayoutContentContainer, botContentFragment).commit();
+
+        //Add Bot Compose Footer Fragment
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        ComposeFooterFragment composeFooterFragment = new ComposeFooterFragment();
+        fragmentTransaction.add(R.id.chatLayoutFooterContainer, composeFooterFragment).commit();
+
+    }
+
+    private void findViews() {
+        chatLayoutFooterContainer = (FrameLayout) findViewById(R.id.chatLayoutFooterContainer);
+        chatLayoutContentContainer = (FrameLayout) findViewById(R.id.chatLayoutContentContainer);
     }
 }
