b1f2b7f29fa6ff59cca1f4565f13fc4faa4244aa
Pradeep Mahato <pradeep.mahato@kore.com>
2016-05-31 19:15:16 +0530

    Updated DimentionUtils

---
+++ BotsSDK/korebot/src/main/java/kore/botssdk/view/viewUtils/DimensionUtil.java
@@ -0,0 +1,23 @@
+package kore.botssdk.view.viewUtils;
+
+import android.content.Context;
+
+import kore.botssdk.utils.Utils;
+
+/**
+ * Created by Pradeep Mahato on 31-May-16.
+ */
+public class DimensionUtil {
+
+    Context context;
+    public static float dp1;
+
+    public DimensionUtil(Context context) {
+        this.context = context;
+    }
+
+    private void init() {
+        dp1 = Utils.convertDpToPixel(context, 1);
+    }
+
+}
