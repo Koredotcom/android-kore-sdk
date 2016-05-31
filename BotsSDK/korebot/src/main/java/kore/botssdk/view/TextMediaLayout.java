8c79f6f3d9c6d438b96c830c8d8b26aca8223ed2
Pradeep Mahato <pradeep.mahato@kore.com>
2016-05-31 19:16:17 +0530

    Created TextMediaLayout containing MediaLayout

---
+++ BotsSDK/korebot/src/main/java/kore/botssdk/view/TextMediaLayout.java
@@ -0,0 +1,149 @@
+package kore.botssdk.view;
+
+import android.content.Context;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import kore.botssdk.R;
+import kore.botssdk.application.AppControl;
+import kore.botssdk.view.viewUtils.LayoutUtils;
+import kore.botssdk.view.viewUtils.MeasureUtils;
+
+/**
+ * Created by Pradeep Mahato on 31-May-16.
+ */
+public class TextMediaLayout extends MediaLayout {
+
+    TextView botContentTextView;
+
+    public static final int TEXTVIEW_ID = 1980081;
+    public static final int TEXT_MEDIA_LAYOUT_ID = 73733614;
+
+    private float restrictedLayoutWidth, restrictedLayoutHeight;
+
+    public static int GRAVITY_LEFT = 0;
+    public static int GRAVITY_RIGHT = 1;
+    int gravity = 0;
+
+    public static int MATCH_PARENT = 0;
+    public static int WRAP_CONTENT = 1;
+    int widthStyle = 0;
+
+    float dp1;
+
+    public TextMediaLayout(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+
+        if (!isInEditMode()) {
+            dp1 = AppControl.getInstance().getDimensionUtil().dp1;
+        }
+
+        //Add a textView
+        botContentTextView = new TextView(getContext());
+        botContentTextView.setLinkTextColor(getResources().getColor(R.color.mentionsAndHashTagColor));
+
+        RelativeLayout.LayoutParams txtVwParams = new RelativeLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        botContentTextView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16);
+        botContentTextView.setText("");
+
+        botContentTextView.setLayoutParams(txtVwParams);
+        botContentTextView.setSingleLine(false);
+        botContentTextView.setClickable(false);
+        botContentTextView.setId(TEXTVIEW_ID);
+        float dp5 = dp1 * 5;
+        botContentTextView.setPadding(0, 0, 0, (int) dp5);
+        if (gravity == GRAVITY_LEFT) {
+            botContentTextView.setGravity(Gravity.LEFT);
+        } else if (gravity == GRAVITY_RIGHT) {
+            botContentTextView.setGravity(Gravity.RIGHT);
+        }
+        botContentTextView.setFocusable(false);
+        botContentTextView.setClickable(false);
+        botContentTextView.setLongClickable(false);
+        addView(botContentTextView);
+
+    }
+
+    public void startup(int position, int... dimens) {
+        populateText(position + "");
+    }
+
+    private void populateText(String textualContent) {
+        botContentTextView.setText(textualContent);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        int childWidthSpec, childHeightSpec;
+        int wrapSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        int containerWidth = 0;
+
+        final int count = getChildCount();
+        int totalHeight = 0;
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            int childHeight = 0, childWidth = 0;
+
+            switch (child.getId()) {
+                case TEXTVIEW_ID:
+                    childWidthSpec = MeasureSpec.makeMeasureSpec((int) restrictedLayoutWidth, MeasureSpec.UNSPECIFIED);
+                    MeasureUtils.measure(child, childWidthSpec, wrapSpec);
+                    childWidth = child.getMeasuredWidth();
+                    if (childWidth > restrictedLayoutWidth) {
+                        childWidthSpec = MeasureSpec.makeMeasureSpec((int) restrictedLayoutWidth, MeasureSpec.AT_MOST);
+                        MeasureUtils.measure(child, childWidthSpec, wrapSpec);
+                    }
+                    childHeight = child.getMeasuredHeight();
+                    break;
+                default:
+
+            }
+
+            totalHeight += childHeight;
+            containerWidth = (containerWidth < child.getMeasuredWidth()) ? child.getMeasuredWidth() : containerWidth;
+        }
+
+        if (widthStyle == MATCH_PARENT) {
+            containerWidth = parentWidth;
+        } else if (widthStyle == WRAP_CONTENT) {
+            containerWidth = containerWidth;
+        }
+
+        int parentHeightSpec = MeasureSpec.makeMeasureSpec(totalHeight, MeasureSpec.EXACTLY);
+        int parentWidthSpec = MeasureSpec.makeMeasureSpec(containerWidth, MeasureSpec.AT_MOST);
+        setMeasuredDimension(parentWidthSpec, parentHeightSpec);
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+        final int count = getChildCount();
+        int parentWidth = getMeasuredWidth();
+
+        //get the available size of child view
+        int childLeft = this.getPaddingLeft();
+        int childTop = this.getPaddingTop();
+
+        //walk through each child, and arrange it from left to right
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() != GONE) {
+                LayoutUtils.layoutChild(child, childLeft, childTop);
+                childTop += child.getMeasuredHeight();
+            }
+        }
+    }
+}
