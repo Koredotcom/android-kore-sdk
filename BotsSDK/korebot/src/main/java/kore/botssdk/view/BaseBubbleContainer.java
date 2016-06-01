9124ee7852293277c9bc1e7f34fb4f3d73fca702
Pradeep Mahato <pradeep.mahato@kore.com>
2016-06-01 10:12:03 +0530

    Base Container layout to hold the bubbles and any more extra views...

---
+++ BotsSDK/korebot/src/main/java/kore/botssdk/view/BaseBubbleContainer.java
@@ -0,0 +1,35 @@
+package kore.botssdk.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by Pradeep Mahato on 01-Jun-16.
+ */
+public abstract class BaseBubbleContainer extends ViewGroup {
+
+    protected int dp1;
+    protected int BUBBLE_CONTENT_LAYOUT_WIDTH, BUBBLE_CONTENT_LAYOUT_HEIGHT;
+
+    public BaseBubbleContainer(Context context) {
+        super(context);
+    }
+
+    public BaseBubbleContainer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaseBubbleContainer(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public BaseBubbleContainer(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public void setDimensions(int containerWidth, int containerHeight) {
+        BUBBLE_CONTENT_LAYOUT_WIDTH = containerWidth;
+        BUBBLE_CONTENT_LAYOUT_HEIGHT = containerHeight;
+    }
+}
