9873bc5f793bcd5b8b06459418ade00a9588e8ba
ramachandra <ramachandra.pradeep.challa@kore.com>
2016-06-01 18:31:30 +0530

    changed service name

--- BotsSDK/korebot/src/main/java/kore/botssdk/activity/BaseSpiceActivity.java
+++ BotsSDK/korebot/src/main/java/kore/botssdk/activity/BaseSpiceActivity.java
@@ -2,14 +2,14 @@ package kore.botssdk.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import com.octo.android.robospice.SpiceManager;
-import kore.botssdk.net.KoreRestService;
+import kore.botssdk.net.KoreBotRestService;
 
 /**
  * Created by Pradeep Mahato on 30-May-16.
  */
 public class BaseSpiceActivity extends AppCompatActivity {
 
-    SpiceManager spiceManager = new SpiceManager(KoreRestService.class);
+    SpiceManager spiceManager = new SpiceManager(KoreBotRestService.class);
 
     @Override
     protected void onStart() {
