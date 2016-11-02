2f0230da952648718006a284098a99407c15cff6
ramachandra <ramachandra.pradeep.challa@kore.com>
2016-11-02 16:55:15 +0530

    removed URL version code

--- BotsSDK/korebotsdklib/src/main/java/kore/botssdk/net/RestAPI.java
+++ BotsSDK/korebotsdklib/src/main/java/kore/botssdk/net/RestAPI.java
@@ -19,37 +19,37 @@ import retrofit.http.Query;
  */
 public interface RestAPI {
 
-    String URL_VERSION = "/1.1";
+//    String URL_VERSION = "/1.1";
 
     /** Login Service **/
-    @POST("/api" + URL_VERSION + "/oAuth/token")
+    @POST("/api/oAuth/token")
     RestResponse.LoginResponse loginUser(@Body HashMap<String, Object> userCredentials);
 
     // Get JWT Token
-    @POST("/api" + URL_VERSION + "/users/sts")
+    @POST("/api/users/sts")
     RestResponse.JWTTokenResponse getJWTToken(@Header("Authorization") String token);
 
     //Getting jwt grant
-    @POST("/api" + URL_VERSION + "/oAuth/token/jwtgrant")
+    @POST("/api/oAuth/token/jwtgrant")
     RestResponse.BotAuthorization jwtGrant(@Body HashMap<String,Object> jwtToken);
 
     //Getting jwt grant Anonymous
-    @POST("/api" + URL_VERSION + "/oAuth/token/jwtgrant/anonymous")
+    @POST("/api/oAuth/token/jwtgrant/anonymous")
     RestResponse.BotAuthorization jwtGrantAnonymous(@Body HashMap<String,Object> jwtToken);
 
     //Getting rtm URL
-    @POST("/api" + URL_VERSION + "/rtm/start")
+    @POST("/api/rtm/start")
     RestResponse.RTMUrl getRtmUrl(@Header("Authorization") String token, @Body HashMap<String, Object> optParameterBotInfo);
 
-    @POST("/api" + URL_VERSION + "/rtm/start")
+    @POST("/api/rtm/start")
     RestResponse.RTMUrl getRtmUrl(@Header("Authorization") String token, @Body HashMap<String, Object> optParameterBotInfo, @Query("isReconnect") boolean isReconnect);
 
     //Get Market Streams
-    @GET("/api" + URL_VERSION + "/users/{userId}/builder/streams")
+    @GET("/api/users/{userId}/builder/streams")
     MarketStreamList getMarketStreams(@Path("userId") String userId, @Header("Authorization") String token);
 
     //Subscribe to Push notification
-    @POST("/api" + URL_VERSION + "/users/{userId}/sdknotifications/subscribe")
+    @POST("/api/users/{userId}/sdknotifications/subscribe")
     Response subscribeForPushNotification(@Path("userId") String userId, @Header("Authorization") String token, @Body HashMap<String, Object> req);
 
     //Unsubscribe to Push notification
@@ -57,7 +57,7 @@ public interface RestAPI {
             "Content-type: application/json",
             "X-HTTP-Method-Override:DELETE"
     })
-    @POST("/api" + URL_VERSION + "/users/{userId}/sdknotifications/unsubscribe")
+    @POST("/api/users/{userId}/sdknotifications/unsubscribe")
     Response unSubscribeForPushNotification(@Path("userId") String userId, @Body HashMap<String, Object> body);
 
 }
