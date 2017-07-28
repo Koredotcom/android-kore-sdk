4ea9af785697857f923527ec27fd09c435521097
Pradeep Mahato <pradeep.mahato@kore.com>
2017-07-28 21:19:36 +0530

    Adding QuickReply recyclerView

---
+++ BotsSDK/korebot/src/main/java/kore/botssdk/adapter/QuickRepliesAdapter.java
@@ -0,0 +1,101 @@
+package kore.botssdk.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import kore.botssdk.R;
+import kore.botssdk.fragment.ComposeFooterFragment;
+import kore.botssdk.listener.InvokeGenericWebViewInterface;
+import kore.botssdk.models.QuickReplyTemplate;
+import kore.botssdk.utils.BundleConstants;
+import kore.botssdk.view.viewHolder.QuickReplyViewHolder;
+
+/**
+ * Created by Pradeep Mahato on 28/7/17.
+ * Copyright (c) 2014 Kore Inc. All rights reserved.
+ */
+public class QuickRepliesAdapter extends RecyclerView.Adapter<QuickReplyViewHolder> {
+
+    private ArrayList<QuickReplyTemplate> quickReplyTemplateArrayList;
+    Context context;
+    LayoutInflater layoutInflater;
+    RecyclerView parentRecyclerView;
+
+    ComposeFooterFragment.ComposeFooterInterface composeFooterInterface;
+    InvokeGenericWebViewInterface invokeGenericWebViewInterface;
+
+    public QuickRepliesAdapter(Context context, RecyclerView parentRecyclerView) {
+        this.context = context;
+        this.parentRecyclerView = parentRecyclerView;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public QuickReplyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View convertView = layoutInflater.inflate(R.layout.quick_reply_item_layout, null);
+        return new QuickReplyViewHolder(convertView);
+    }
+
+    @Override
+    public void onBindViewHolder(QuickReplyViewHolder holder, int position) {
+        QuickReplyTemplate quickReplyTemplate = quickReplyTemplateArrayList.get(position);
+
+        if (quickReplyTemplate.getImage_url() != null) {
+            Picasso.with(context).load(quickReplyTemplate.getImage_url()).into(holder.getQuickReplyImage());
+            holder.getQuickReplyImage().setVisibility(View.VISIBLE);
+        } else {
+            holder.getQuickReplyImage().setVisibility(View.INVISIBLE);
+        }
+
+        holder.getQuickReplyTitle().setText(quickReplyTemplate.getTitle());
+
+        holder.getQuickReplyRoot().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position =  parentRecyclerView.getChildPosition(v);
+                if (composeFooterInterface != null && invokeGenericWebViewInterface != null) {
+                    QuickReplyTemplate quickReplyTemplate = quickReplyTemplateArrayList.get(position);
+                    if (BundleConstants.BUTTON_TYPE_POSTBACK.equalsIgnoreCase(quickReplyTemplate.getContent_type())) {
+                        invokeGenericWebViewInterface.invokeGenericWebView(quickReplyTemplate.getPayload());
+                    } else {
+                        composeFooterInterface.onSendClick(quickReplyTemplate.getPayload());
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+
+        if (quickReplyTemplateArrayList == null) {
+            return 0;
+        } else {
+            return quickReplyTemplateArrayList.size();
+        }
+    }
+
+    public void setQuickReplyTemplateArrayList(ArrayList<QuickReplyTemplate> quickReplyTemplateArrayList) {
+        this.quickReplyTemplateArrayList = quickReplyTemplateArrayList;
+    }
+
+    public void setComposeFooterInterface(ComposeFooterFragment.ComposeFooterInterface composeFooterInterface) {
+        this.composeFooterInterface = composeFooterInterface;
+    }
+
+    public void setInvokeGenericWebViewInterface(InvokeGenericWebViewInterface invokeGenericWebViewInterface) {
+        this.invokeGenericWebViewInterface = invokeGenericWebViewInterface;
+    }
+}
---
+++ BotsSDK/korebot/src/main/java/kore/botssdk/view/viewHolder/QuickReplyViewHolder.java
@@ -0,0 +1,38 @@
+package kore.botssdk.view.viewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import kore.botssdk.R;
+
+/**
+ * Created by Pradeep Mahato on 28/7/17.
+ * Copyright (c) 2014 Kore Inc. All rights reserved.
+ */
+public class QuickReplyViewHolder extends RecyclerView.ViewHolder{
+    TextView quickReplyTitle;
+    ImageView quickReplyImage;
+    LinearLayout quickReplyRoot;
+
+    public QuickReplyViewHolder(View view) {
+        super(view);
+        quickReplyImage = (ImageView) view.findViewById(R.id.quick_reply_item_image);
+        quickReplyTitle = (TextView) view.findViewById(R.id.quick_reply_item_text);
+        quickReplyRoot = (LinearLayout) view.findViewById(R.id.quick_reply_item_root);
+    }
+
+    public TextView getQuickReplyTitle() {
+        return quickReplyTitle;
+    }
+
+    public ImageView getQuickReplyImage() {
+        return quickReplyImage;
+    }
+
+    public LinearLayout getQuickReplyRoot() {
+        return quickReplyRoot;
+    }
+}
