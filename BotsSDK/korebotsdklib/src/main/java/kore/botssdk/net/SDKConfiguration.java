3bceff74d686b1a82b4525a8652b78b6b4cd94e2
Shiva krishna Kongara <shiva.kongara@kore.com>
2018-11-27 13:12:26 +0530

    removed time stamps

--- BotsSDK/korebotsdklib/src/main/java/kore/botssdk/net/SDKConfiguration.java
+++ BotsSDK/korebotsdklib/src/main/java/kore/botssdk/net/SDKConfiguration.java
@@ -167,7 +167,7 @@ public class SDKConfiguration {
         TIME_STAMPS_REQUIRED = timeStampsRequired;
     }
 
-    private static boolean TIME_STAMPS_REQUIRED = true;
+    private static boolean TIME_STAMPS_REQUIRED = false;
     private static boolean APPLY_FONT_STYLE = true;
 
 
