fda8d7682ed66e4beb80fb67c77b10a20e4205e8
shivakcpm <shivakrishna.kongara@gmail.com>
2019-04-08 17:50:08 +0530

    Meetings changes for templates

--- BotsSDK/korebot/src/main/java/kore/botssdk/view/MeetingConfirmationView.java
+++ BotsSDK/korebot/src/main/java/kore/botssdk/view/MeetingConfirmationView.java
@@ -18,28 +18,25 @@ import kore.botssdk.application.AppControl;
 import kore.botssdk.listener.ComposeFooterInterface;
 import kore.botssdk.listener.InvokeGenericWebViewInterface;
 import kore.botssdk.models.MeetingConfirmationModel;
+import kore.botssdk.models.MeetingSlotModel;
 import kore.botssdk.utils.DateUtils;
 import kore.botssdk.utils.StringUtils;
 import kore.botssdk.view.viewUtils.LayoutUtils;
 import kore.botssdk.view.viewUtils.MeasureUtils;
 
+import static kore.botssdk.utils.DateUtils.getSlotsDate;
 import static kore.botssdk.utils.DateUtils.getTimeInAmPm;
 
 public class MeetingConfirmationView extends ViewGroup {
     private TextView locationView;
     private TextView titleView;
-    private TextView dateView, tv_time, tv_users;
-    LinearLayout lin_timeview, linlocation;
+    private TextView tv_users;
     private View slotLayout;
+    private TextView label;
+    private TextView slots;
 
     float dp1;
-    private float restrictedLayoutWidth;
-    private Drawable backgroundDrawable;
 
-
-    public void setRestrictedLayoutWidth(float restrictedLayoutWidth) {
-        this.restrictedLayoutWidth = restrictedLayoutWidth;
-    }
 
     public MeetingConfirmationView(Context context) {
         super(context);
@@ -79,21 +76,13 @@ public class MeetingConfirmationView extends ViewGroup {
 
     private void init() {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.meeting_confirmation_layout, this, true);
-        //RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
-        //recyclerView.setLayoutManager(layoutManager);
-        //recyclerView.setItemAnimator(new DefaultItemAnimator());
-        // KaFontUtils.applyCustomFont(getContext(), view);
         locationView = (TextView) view.findViewById(R.id.location_view);
         slotLayout = view.findViewById(R.id.slot_confirm_layout);
         titleView = (TextView) view.findViewById(R.id.title_view);
-        dateView = (TextView) view.findViewById(R.id.date_view);
         tv_users = (TextView) view.findViewById(R.id.tv_users);
-        tv_time = (TextView) view.findViewById(R.id.tv_time);
-        lin_timeview = (LinearLayout) view.findViewById(R.id.lin_timeview);
-        linlocation = (LinearLayout) view.findViewById(R.id.linlocation);
         dp1 = (int) AppControl.getInstance().getDimensionUtil().dp1;
-        //backgroundDrawable = getContext().getResources().getDrawable(R.drawable.bottom_right_rounded_rectangle);
-        // slotLayout.setBackground(backgroundDrawable);
+        label = view.findViewById(R.id.label);
+        slots = view.findViewById(R.id.time_slots);
     }
 
 
@@ -108,7 +97,7 @@ public class MeetingConfirmationView extends ViewGroup {
 
         childWidthSpec = MeasureSpec.makeMeasureSpec((int) (parentWidth - 28 * dp1), MeasureSpec.EXACTLY);
         MeasureUtils.measure(slotLayout, childWidthSpec, wrapSpec);
-        if(slotLayout.getVisibility() == VISIBLE ) {
+        if (slotLayout.getVisibility() == VISIBLE) {
             totalHeight += slotLayout.getMeasuredHeight() + getPaddingBottom() + getPaddingTop();
         }
         int parentHeightSpec = MeasureSpec.makeMeasureSpec(totalHeight, MeasureSpec.EXACTLY);
@@ -125,38 +114,20 @@ public class MeetingConfirmationView extends ViewGroup {
             if (!StringUtils.isNullOrEmptyWithTrim(meetingConfirmationModel.getWhere())) {
                 locationView.setText(meetingConfirmationModel.getWhere());
                 locationView.setVisibility(VISIBLE);
-                linlocation.setVisibility(VISIBLE);
             } else {
                 locationView.setVisibility(GONE);
-                linlocation.setVisibility(GONE);
             }
 
             tv_users.setText(TextUtils.isEmpty(getFormatedAttendiesFromList(meetingConfirmationModel.getAttendees())) ? ""
                     : getFormatedAttendiesFromList(meetingConfirmationModel.getAttendees()));
 
 
-            if (meetingConfirmationModel.getDate() > 0 &&
-                    meetingConfirmationModel.getSlot_end() > 0 &&
-                    meetingConfirmationModel.getSlot_start() > 0) {
-
-                String startTime = getTimeInAmPm(meetingConfirmationModel.getSlot_start()).toUpperCase();
-                String endTime = getTimeInAmPm(meetingConfirmationModel.getSlot_end()).toUpperCase();
-                //  dateView.setText(MessageFormat.format("{0}, {1} to {2} ", DateUtils.getDate(meetingConfirmationModel.getDate()), startTime, endTime));
-                //dateView.setText(MessageFormat.format("{0}", DateUtils.getDateinMeetingFormat(meetingConfirmationModel.getDate()).toUpperCase()));
-
-                String date = DateUtils.getDay(meetingConfirmationModel.getDate()).toUpperCase();
-                dateView.setText(date);
-
-
-                tv_time.setText(startTime + "\n" + endTime);
-                dateView.setVisibility(VISIBLE);
-                tv_time.setVisibility(VISIBLE);
+            if (meetingConfirmationModel.getSlots() != null && meetingConfirmationModel.getSlots().size() > 0) {
+                slots.setText(getSlotsViewText(meetingConfirmationModel.getSlots()));
+                label.setText(meetingConfirmationModel.getSlots().size() > 1 ? "Selected Slots" : "Selected Slot");
             } else {
-                dateView.setVisibility(GONE);
-                tv_time.setVisibility(GONE);
-                lin_timeview.setVisibility(GONE);
-
-
+                slots.setText("");
+                label.setText("");
             }
             //GON
         } else {
@@ -165,34 +136,14 @@ public class MeetingConfirmationView extends ViewGroup {
 
     }
 
-   /* private String getAttendies(ArrayList<String> list) {
-        String users = "";
-        if (list != null && list.size() > 0) {
-
-
-            for (int i = 0; i < list.size(); i++) {
-
-                if (list.size() == 1) {
-                    users += list.get(i);
-                    return users;
-                } else if (i > 2) {
-                    int remaining_users = list.size() - 3;
-                    users = String.format(users + " and %1$d others", remaining_users);
-                    return users;
-                } else if (i == (list.size() - 1)) {
-                    users += " And " + list.get(i);
-                } else {
-                    users += TextUtils.isEmpty(users) ? "" : ", ";
-                    users += list.get(i);
-                }
-                //  users.substring(0, users.length() - 2);
-            }
-
+    private String getSlotsViewText(ArrayList<MeetingSlotModel.Slot> slots) {
+        StringBuilder message = new StringBuilder();
+        for (MeetingSlotModel.Slot slot : slots) {
+            message.append(getSlotsDate(slot.getStart())).append(", ").append(DateUtils.getTimeInAmPm(slot.getStart())).append(" to ").append(DateUtils.getTimeInAmPm(slot.getEnd())).append("\n");
         }
+        return message.toString().trim();
+    }
 
-
-        return users;
-    }*/
 
 
     private String getFormatedAttendiesFromList(ArrayList<MeetingConfirmationModel.UserDetailModel> userDetailModels) {
@@ -200,25 +151,13 @@ public class MeetingConfirmationView extends ViewGroup {
         if (userDetailModels != null && userDetailModels.size() > 0) {
             if (userDetailModels.size() == 1) {
                 return userDetailModels.get(0).getFirstName();
-            }
-            else {
+            } else {
                 int remaining = userDetailModels.size() - 1;
                 return String.format("%1$s  and %2$d others", userDetailModels.get(0).getFirstName(), remaining);
 
             }
-                /*else if (userDetailModels.size() == 2) {
 
-                return String.format("%1$s and %2$s", userDetailModels.get(0).getFirstName(), userDetailModels.get(1).getFirstName());
-            } else if (userDetailModels.size() == 3) {
-
-                return String.format("%1$s , %2$s and %3$s", userDetailModels.get(0).getFirstName(), userDetailModels.get(1).getFirstName(), userDetailModels.get(2).getFirstName());
-            } else {
-                int remaining = userDetailModels.size() - 3;
-                return String.format("%1$s , %2$s , %3$s and %4$d others", userDetailModels.get(0).getFirstName(),
-                        userDetailModels.get(1).getFirstName(), userDetailModels.get(2).getFirstName(), remaining);
-            }*/
         }
-
 
 
         return "";
--- BotsSDK/korebot/src/main/java/kore/botssdk/view/MeetingSlotsView.java
+++ BotsSDK/korebot/src/main/java/kore/botssdk/view/MeetingSlotsView.java
@@ -13,13 +13,14 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import kore.botssdk.R;
 import kore.botssdk.adapter.MeetingSlotsButtonAdapter;
 import kore.botssdk.application.AppControl;
-import kore.botssdk.fragment.ComposeFooterFragment;
 import kore.botssdk.listener.ComposeFooterInterface;
 import kore.botssdk.listener.InvokeGenericWebViewInterface;
+import kore.botssdk.models.BotButtonModel;
 import kore.botssdk.models.BotResponse;
 import kore.botssdk.models.MeetingSlotModel;
 import kore.botssdk.models.MeetingTemplateModel;
@@ -28,9 +29,9 @@ import kore.botssdk.utils.KaFontUtils;
 import kore.botssdk.view.viewUtils.LayoutUtils;
 import kore.botssdk.view.viewUtils.MeasureUtils;
 
-public class MeetingSlotsView extends ViewGroup{
+public class MeetingSlotsView extends ViewGroup {
     private AutoExpandListView autoExpandListView;
-    private TextView showMore;
+    private TextView button1, button2;
     private View meetingLayout;
 
     float dp1;
@@ -56,10 +57,10 @@ public class MeetingSlotsView extends ViewGroup{
         super(context, attrs, defStyleAttr);
         init();
     }
+
     public InvokeGenericWebViewInterface getInvokeGenericWebViewInterface() {
         return invokeGenericWebViewInterface;
     }
-
 
 
     public void setInvokeGenericWebViewInterface(InvokeGenericWebViewInterface invokeGenericWebViewInterface) {
@@ -78,79 +79,98 @@ public class MeetingSlotsView extends ViewGroup{
     private ComposeFooterInterface composeFooterInterface;
 
 
-
     private void init() {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.meeting_slots_template, this, true);
         autoExpandListView = (AutoExpandListView) view.findViewById(R.id.slots_list);
-        KaFontUtils.applyCustomFont(getContext(),view);
+        KaFontUtils.applyCustomFont(getContext(), view);
         meetingLayout = view.findViewById(R.id.meeting_layout);
-        showMore = (TextView) view.findViewById(R.id.show_more_view);
+        button1 = (TextView) view.findViewById(R.id.button1);
+        button2 = view.findViewById(R.id.button2);
         dp1 = (int) AppControl.getInstance().getDimensionUtil().dp1;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            showMore.setLetterSpacing(0.06f);
+            button1.setLetterSpacing(0.06f);
+            button2.setLetterSpacing(0.06f);
         }
     }
 
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
         int wrapSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+
 
         int totalHeight = getPaddingTop();
         int childWidthSpec;
 
-        childWidthSpec = MeasureSpec.makeMeasureSpec((int) restrictedLayoutWidth, MeasureSpec.UNSPECIFIED);
+        childWidthSpec = MeasureSpec.makeMeasureSpec((int) (parentWidth - 28 * dp1), MeasureSpec.EXACTLY);
         MeasureUtils.measure(meetingLayout, childWidthSpec, wrapSpec);
-        int  childWidth = meetingLayout.getMeasuredWidth();
-        if (childWidth > restrictedLayoutWidth) {
-            childWidthSpec = MeasureSpec.makeMeasureSpec((int) restrictedLayoutWidth, MeasureSpec.AT_MOST);
-            MeasureUtils.measure(meetingLayout, childWidthSpec, wrapSpec);
-        }
-        totalHeight += meetingLayout.getMeasuredHeight()+getPaddingBottom()+getPaddingTop();
-        int parentHeightSpec = MeasureSpec.makeMeasureSpec( totalHeight, MeasureSpec.EXACTLY);
+
+        totalHeight += meetingLayout.getMeasuredHeight() + getPaddingBottom() + getPaddingTop();
+
+        int parentHeightSpec = MeasureSpec.makeMeasureSpec(totalHeight, MeasureSpec.EXACTLY);
         int parentWidthSpec = MeasureSpec.makeMeasureSpec(meetingLayout.getMeasuredWidth(), MeasureSpec.AT_MOST);
         setMeasuredDimension(parentWidthSpec, parentHeightSpec);
     }
 
 
-    public void populateData(final MeetingTemplateModel meetingTemplateModel, boolean isEnabled){
+    public void populateData(final MeetingTemplateModel meetingTemplateModel, boolean isEnabled) {
 
-        if(meetingTemplateModel != null) {
+        if (meetingTemplateModel != null) {
             meetingLayout.setVisibility(VISIBLE);
-            ArrayList<MeetingSlotModel.Slot> slots = meetingTemplateModel != null ? meetingTemplateModel.getQuick_slots() : new ArrayList<MeetingSlotModel.Slot>();
+            ArrayList<MeetingSlotModel.Slot> slots = meetingTemplateModel.getQuick_slots();
             MeetingSlotsButtonAdapter slotsButtonAdapter = new MeetingSlotsButtonAdapter(getContext());
             slotsButtonAdapter.setMeetingsModelArrayList(slots);
             slotsButtonAdapter.setEnabled(isEnabled);
             slotsButtonAdapter.setComposeFooterInterface(composeFooterInterface);
             autoExpandListView.setAdapter(slotsButtonAdapter);
             slotsButtonAdapter.notifyDataSetChanged();
-            showMore.setVisibility(slots.size() > 0 && meetingTemplateModel != null && meetingTemplateModel.isShowMore() ? VISIBLE : GONE);
-            meetingLayout.setAlpha(isEnabled ? 1.0f : 0.5f);
-            showMore.setTextColor(getResources().getColor(isEnabled ? R.color.splash_color : R.color.color_a7b0be));
-            ((GradientDrawable) showMore.getBackground()).setStroke(2,isEnabled ?getResources().getColor(R.color.splash_color) : getResources().getColor(R.color.color_a7b0be));
+            ArrayList<BotButtonModel> buttonModels = meetingTemplateModel.getButtons();
+            if (buttonModels != null && buttonModels.size() > 0) {
 
-            showMore.setOnClickListener(isEnabled ? new OnClickListener() {
+                if (buttonModels.size() >= 2) {
+                    button1.setText(buttonModels.get(0).getTitle());
+                    button2.setText(buttonModels.get(1).getTitle());
+
+                    button1.setVisibility(VISIBLE);
+                    button2.setVisibility(VISIBLE);
+                }else if(meetingTemplateModel.getButtons().size() ==1){
+                    button1.setVisibility(VISIBLE);
+                    button1.setText(buttonModels.get(0).getTitle());
+                }
+            }else{
+                button1.setVisibility(GONE);
+                button2.setVisibility(GONE);
+            }
+            meetingLayout.setAlpha(isEnabled ? 1.0f : 0.5f);
+            button1.setTextColor(getResources().getColor(isEnabled ? R.color.splash_color : R.color.color_a7b0be));
+            button2.setTextColor(getResources().getColor(isEnabled ? R.color.splash_color : R.color.color_a7b0be));
+            ((GradientDrawable) button1.getBackground()).setStroke(2, isEnabled ? getResources().getColor(R.color.splash_color) : getResources().getColor(R.color.color_a7b0be));
+            ((GradientDrawable) button2.getBackground()).setStroke(2, isEnabled ? getResources().getColor(R.color.splash_color) : getResources().getColor(R.color.color_a7b0be));
+
+            button1.setOnClickListener(isEnabled ? new OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Bundle bundle = new Bundle();
-                    bundle.putString(BundleConstants.SLOTS_LIST, gson.toJson(meetingTemplateModel.getWorking_hours()));
-                    bundle.putString(BundleConstants.QUICK_SLOTS,  gson.toJson(meetingTemplateModel.getQuick_slots()));
-                    composeFooterInterface.launchActivityWithBundle(BotResponse.TEMPLATE_TYPE_SLOT_PICKER, bundle);
+                    composeFooterInterface.onSendClick(buttonModels.get(0).getPayload());
                 }
             } : null);
-        }else{
+            button2.setOnClickListener(isEnabled ? new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    composeFooterInterface.onSendClick(buttonModels.get(1).getPayload());}
+            } : null);
+        } else {
             autoExpandListView.setAdapter(null);
             meetingLayout.setVisibility(GONE);
         }
     }
 
 
-
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
         final int count = getChildCount();
-        int parentWidth = getMeasuredWidth();
+
 
         //get the available size of child view
         int childLeft = 0;
