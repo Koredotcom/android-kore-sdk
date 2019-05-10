d9a91d12ceeb9c38de1e39c49b1cc20d03862940
Shridhar <shridhar.metraskar@kore.com>
2019-05-10 19:33:14 +0530

    Email Action Completion

---
+++ BotsSDK/korebotsdklib/src/main/java/kore/botssdk/models/CreateEmailActionRequest.java
@@ -0,0 +1,14 @@
+package kore.botssdk.models;
+
+
+public class CreateEmailActionRequest {
+    String subDomain;
+
+    public String getSubDomain() {
+        return subDomain;
+    }
+
+    public void setSubDomain(String subDomain) {
+        this.subDomain = subDomain;
+    }
+}
---
+++ BotsSDK/korebotsdklib/src/main/java/kore/botssdk/models/EmailSuggestionModel.java
@@ -0,0 +1,23 @@
+package kore.botssdk.models;
+
+public class EmailSuggestionModel {
+
+    String suggestion;
+    String domain;
+
+    public String getSuggestion() {
+        return suggestion;
+    }
+
+    public void setSuggestion(String suggestion) {
+        this.suggestion = suggestion;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+}
