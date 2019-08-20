be16e3595769e3208196d14db062f1095d19e4d8
ramachandra <ramachandra.pradeep.challa@kore.com>
2019-08-20 14:59:39 +0530

    making skill name bold

--- BotsSDK/korebotsdklib/src/main/java/kore/botssdk/utils/DialogCaller.java
+++ BotsSDK/korebotsdklib/src/main/java/kore/botssdk/utils/DialogCaller.java
@@ -3,6 +3,7 @@ package kore.botssdk.utils;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.text.Html;
 
 public class DialogCaller {
 
@@ -13,8 +14,8 @@ public static void showDialog(Context context,
   //  dialog.setTitle(Constants.SKILL_HOME);
     dialog.setCancelable(false);
 //    Updated text - This action will end your conversation with <skill name> and move to Kora. Do you want to continue? (Yes) (No)
-    dialog.setMessage("This action will end your conversation with " + Constants.SKILL_SELECTION +
-            " skill and move to Kora. Do you want to continue?");
+    dialog.setMessage(Html.fromHtml("This action will end your conversation with <b>" + Constants.SKILL_SELECTION +
+            "</b> skill and move to Kora. Do you want to continue?"));
   //  dialog.setMessage("You are conversing with " + Constants.SKILL_HOME + " skill now. Clicking on this will end your conversation with " + Constants.SKILL_SELECTION + " skill and move to Kora skill ");
     dialog.setPositiveButton("Yes",onClickListener);
     dialog.setNegativeButton("No",null);
