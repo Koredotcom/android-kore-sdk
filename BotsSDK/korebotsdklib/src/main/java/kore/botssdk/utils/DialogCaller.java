bb0f0d6b1c57e58c55bdeeb13f0034033913d571
Shridhar <shridhar.metraskar@kore.com>
2019-08-20 18:10:09 +0530

    Skill switch message changed

--- BotsSDK/korebotsdklib/src/main/java/kore/botssdk/utils/DialogCaller.java
+++ BotsSDK/korebotsdklib/src/main/java/kore/botssdk/utils/DialogCaller.java
@@ -11,12 +11,14 @@ public static void showDialog(Context context,
                               DialogInterface.OnClickListener onClickListener) {
 
     AlertDialog.Builder dialog = new AlertDialog.Builder(context);
-  //  dialog.setTitle(Constants.SKILL_HOME);
     dialog.setCancelable(false);
-//    Updated text - This action will end your conversation with <skill name> and move to Kora. Do you want to continue? (Yes) (No)
     dialog.setMessage(Html.fromHtml("This action will end your conversation with <b>" + Constants.SKILL_SELECTION +
-            "</b> skill and move to Kora. Do you want to continue?"));
-  //  dialog.setMessage("You are conversing with " + Constants.SKILL_HOME + " skill now. Clicking on this will end your conversation with " + Constants.SKILL_SELECTION + " skill and move to Kora skill ");
+            "</b> and move to "+ Constants.SKILL_HOME+". Do you want to continue?"));
+
+
+
+   /* dialog.setMessage(Html.fromHtml("This action will end your conversation with <b>" + Constants.SKILL_SELECTION +
+            "</b> skill and move to Kora. Do you want to continue?"));*/
     dialog.setPositiveButton("Yes",onClickListener);
     dialog.setNegativeButton("No",null);
     dialog.show();
