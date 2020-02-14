8d0ee519d39a3a81edcc9c35fb2ae23462ac69d9
koreanilkumar <anilkumar.routhu@kore.com>
2020-02-14 16:14:15 +0530

    bmc to kora required changes

--- BotsSDK/korebotsdklib/src/main/java/kore/botssdk/utils/Constants.java
+++ BotsSDK/korebotsdklib/src/main/java/kore/botssdk/utils/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
 
     //Skill switch name;
     public static  String  SKILL_SELECTION="";
-    public static final String SKILL_HOME="BMC Assistant";
-    public static final String SKILL_UTTERANCE="Ask BMC Assistant ";
+    public static final String SKILL_HOME="Kora";
+    public static final String SKILL_UTTERANCE="Ask Kora ";
 
 //    public static final String PUSH_NOTIF_OS_TYPE_ANDROID = "android";
 //    public static final String PUSH_NOTIF_DEVICE_ID = "deviceId";
