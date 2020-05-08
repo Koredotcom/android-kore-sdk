6ee911d503097b4f7d451222c6b28ad62cd1d31c
Shridhar <shridhar.metraskar@kore.com>
2020-05-08 10:51:39 +0530

    Calendar notes added

--- BotsSDK/korebot/src/main/java/kore/botssdk/adapter/CalendarMeetingNotesAdapter.java
+++ BotsSDK/korebot/src/main/java/kore/botssdk/adapter/CalendarMeetingNotesAdapter.java
@@ -1,11 +1,14 @@
 package kore.botssdk.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -17,9 +20,13 @@ import kore.botssdk.models.CalEventsTemplateModel;
 import kore.botssdk.utils.DateUtils;
 import kore.botssdk.utils.Utility;
 import kore.botssdk.utils.WidgetViewMoreEnum;
+import kore.botssdk.view.viewHolder.CalendarMeetingNotesViewHolder;
+import kore.botssdk.view.viewHolder.EmptyWidgetViewHolder;
 import kore.botssdk.view.viewHolder.MeetingNotesViewHolder;
 
-public class CalendarMeetingNotesAdapter extends RecyclerView.Adapter<MeetingNotesViewHolder> implements RecyclerViewDataAccessor {
+import static android.view.View.GONE;
+
+public class CalendarMeetingNotesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements RecyclerViewDataAccessor {
 
     Context context;
     ArrayList<CalEventsTemplateModel> modelData;
@@ -31,30 +38,52 @@ public class CalendarMeetingNotesAdapter extends RecyclerView.Adapter<MeetingNot
 
     @NonNull
     @Override
-    public MeetingNotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(context).inflate(R.layout.us_meeting_layout, parent, false);
-        return new MeetingNotesViewHolder(view);
+        if(viewType==DATA_FOUND) {
+            View view = LayoutInflater.from(context).inflate(R.layout.cal_meeting_notes_layout, parent, false);
+            return new CalendarMeetingNotesViewHolder(view);
+        }
+        else {
+
+            View view = LayoutInflater.from(context).inflate(R.layout.card_empty_widget_layout, parent, false);
+            return new EmptyWidgetViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MeetingNotesViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holderOrig, int position) {
 
+        if(holderOrig instanceof CalendarMeetingNotesViewHolder) {
+            CalendarMeetingNotesViewHolder holder=(CalendarMeetingNotesViewHolder)holderOrig;
+            CalEventsTemplateModel model = modelData.get(position);
+            holder.date_view.setText(DateUtils.getDateMMMDDYYYY(model.getDuration().getStart(), model.getDuration().getEnd()));
+            String text = Utility.getFormatedAttendiesFromList(model.getAttendees());
+            holder.creator_view.setText(model.getnNotes());
+            holder.title_view.setText(model.getTitle());
+            holder.divider.setVisibility(View.VISIBLE);
+            if(model.getColor()!=null)
+                holder.sidebar.setBackgroundColor(Color.parseColor(model.getColor()));
 
-        CalEventsTemplateModel model = modelData.get(position);
-        holder.header.setVisibility(View.GONE);
-        holder.date_view.setText(DateUtils.getDateMMMDDYYYY(model.getDuration().getStart(), model.getDuration().getEnd()));
-        String text = Utility.getFormatedAttendiesFromList(model.getAttendees());
-        holder.creator_view.setText(text);
-        holder.title_view.setText(model.getTitle());
-        holder.divider.setVisibility(View.VISIBLE);
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-                verticalListViewActionHelper.meetingNotesNavigation(context, model.getMeetingNoteId(), model.getEventId());
+                    verticalListViewActionHelper.meetingNotesNavigation(context, model.getMeetingNoteId(), model.getEventId());
+                }
+            });
+        }
+        else {
+            EmptyWidgetViewHolder emptyHolder = (EmptyWidgetViewHolder) holderOrig;
+
+            emptyHolder.tv_disrcription.setText(msg != null ? msg : "No Meeting Notes");
+
+            emptyHolder.img_icon.setImageDrawable(holderOrig.getItemViewType() == EMPTY_CARD ? ContextCompat.getDrawable(context, R.drawable.no_meeting) : errorIcon);
+            if (errorIcon == null) {
+                emptyHolder.img_icon.setVisibility(GONE);
             }
-        });
+
+        }
     }
 
     @Override
@@ -67,12 +96,33 @@ public class CalendarMeetingNotesAdapter extends RecyclerView.Adapter<MeetingNot
         return modelData != null ? modelData.size() : 0;*/
 
         if (widgetViewMoreEnum!=null&&widgetViewMoreEnum==WidgetViewMoreEnum.EXPAND_VIEW) {
-            return modelData != null && modelData.size() > 0 ? modelData.size() : 0;
+            return modelData != null && modelData.size() > 0 ? modelData.size() : 1;
         }
-        return modelData != null && modelData.size() > 0 ? (isFromWidget && modelData.size() > 3 ? 3 : modelData.size()) : 0;
+        return modelData != null && modelData.size() > 0 ? (isFromWidget && modelData.size() > 3 ? 3 : modelData.size()) : 1;
 
     }
 
+    public void setMessage(String msg, Drawable errorIcon) {
+        this.msg = msg;
+        this.errorIcon = errorIcon;
+    }
+    String msg;
+    Drawable errorIcon;
+    private int DATA_FOUND = 1;
+    private int EMPTY_CARD = 0;
+    private int MESSAGE = 2;
+    @Override
+    public int getItemViewType(int position) {
+
+        if(modelData!=null&&modelData.size()>0)
+        {
+            return DATA_FOUND;
+        }
+        if (msg != null && !msg.equalsIgnoreCase("")) {
+            return MESSAGE;
+        }
+        return EMPTY_CARD;
+    }
 
     @Override
     public ArrayList getData() {
