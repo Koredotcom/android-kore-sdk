940d66f5a8e6428d30acfe5f2135ee35f104c4c7
SudheerJa-Kore <sudheer.jampana@kore.com>
2020-10-22 16:40:06 +0530

    findly_events

---
+++ BotsSDK/findlysdk/src/main/java/com/kore/findlysdk/events/SetStateOfPanel.java
@@ -0,0 +1,5 @@
+package com.kore.findlysdk.events;
+
+public class SetStateOfPanel {
+    public SetStateOfPanel(){}
+}
---
+++ BotsSDK/findlysdk/src/main/java/com/kore/findlysdk/events/SocketDataTransferModel.java
@@ -0,0 +1,56 @@
+package com.kore.findlysdk.events;
+
+import com.kore.findlysdk.listners.BaseSocketConnectionManager;
+import com.kore.findlysdk.models.BotRequest;
+
+/**
+ * Created by Ramachandra Pradeep on 03-Jan-18.
+ */
+
+public class SocketDataTransferModel {
+    public BaseSocketConnectionManager.EVENT_TYPE getEvent_type() {
+        return event_type;
+    }
+
+    public void setEvent_type(BaseSocketConnectionManager.EVENT_TYPE event_type) {
+        this.event_type = event_type;
+    }
+
+    public String getPayLoad() {
+        return payLoad;
+    }
+
+    public void setPayLoad(String payLoad) {
+        this.payLoad = payLoad;
+    }
+
+    private BaseSocketConnectionManager.EVENT_TYPE event_type;
+    private String payLoad;
+
+    public BotRequest getBotRequest() {
+        return botRequest;
+    }
+
+    public void setBotRequest(BotRequest botRequest) {
+        this.botRequest = botRequest;
+    }
+
+    private BotRequest botRequest;
+
+    public boolean isFromSkillSwitch() {
+        return isFromSkillSwitch;
+    }
+
+    public void setFromUtterance(boolean isFromSkillSwitch) {
+        this.isFromSkillSwitch = isFromSkillSwitch;
+    }
+
+    private boolean isFromSkillSwitch = false;
+
+    public SocketDataTransferModel(BaseSocketConnectionManager.EVENT_TYPE event_type, String payLoad, BotRequest botRequest, boolean isFromSkillSwitch){
+        this.event_type = event_type;
+        this.payLoad = payLoad;
+        this.botRequest = botRequest;
+        this.isFromSkillSwitch = isFromSkillSwitch;
+    }
+}
