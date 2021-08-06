615b297bc2f0961a63db34efe1eb3271179b9136
ShridharMe-Kore <shridhar.metraskar@kore.com>
2021-08-06 12:29:34 +0530

    markdown cache issue fix-cherry picked

--- BotsSDK/korebotsdklib/src/main/java/kore/botssdk/utils/markdown/MarkdownImageTagHandler.java
+++ BotsSDK/korebotsdklib/src/main/java/kore/botssdk/utils/markdown/MarkdownImageTagHandler.java
@@ -6,10 +6,21 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LevelListDrawable;
 import android.os.AsyncTask;
 import android.text.Html;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -25,88 +36,122 @@ public class MarkdownImageTagHandler implements Html.ImageGetter {
     private TextView htmlTextViewRemote;
     private String htmlStringRemote;
     MarkDownImageClick markDownImageClick;
-    public MarkdownImageTagHandler(Context context, TextView htmlTextViewRemote, String htmlStringRemote,MarkDownImageClick markDownImageClick) {
+
+    public MarkdownImageTagHandler(Context context, TextView htmlTextViewRemote, String htmlStringRemote, MarkDownImageClick markDownImageClick) {
         this.context = context;
         this.htmlTextViewRemote = htmlTextViewRemote;
         this.htmlStringRemote = htmlStringRemote;
-        this.markDownImageClick=markDownImageClick;
+        this.markDownImageClick = markDownImageClick;
     }
 
     @Override
     public Drawable getDrawable(String source) {
-        HttpGetDrawableTask httpGetDrawableTask = new HttpGetDrawableTask(context, htmlTextViewRemote, htmlStringRemote,markDownImageClick);
+/*        HttpGetDrawableTask httpGetDrawableTask = new HttpGetDrawableTask(context, htmlTextViewRemote, htmlStringRemote,markDownImageClick);
         httpGetDrawableTask.execute(source);
-        return null;
-    }
-}
+        return null;*/
+        final LevelListDrawable drawable = new LevelListDrawable();
 
-class HttpGetDrawableTask extends AsyncTask<String, Void, Drawable> {
-
-    private Context context;
-    private TextView taskTextView;
-    private String taskHtmlString;
-    MarkDownImageClick markDownImageClick;
-    String url;
-    HttpGetDrawableTask(Context context, TextView v, String s,MarkDownImageClick markDownImageClick) {
-        this.context = context;
-        taskTextView = v;
-        taskHtmlString = s;
-        this.markDownImageClick=markDownImageClick;
-    }
-
-    @Override
-    protected Drawable doInBackground(String... params) {
-        Drawable drawable = null;
-        URL sourceURL;
-        try {
-            sourceURL = new URL(params[0]);
-            url=params[0];
-            URLConnection urlConnection = sourceURL.openConnection();
-            urlConnection.connect();
-
-            InputStream inputStream = urlConnection.getInputStream();
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(
-                    inputStream);
-            Bitmap bm = BitmapFactory.decodeStream(bufferedInputStream);
-
-            // convert Bitmap to Drawable
-            drawable = new BitmapDrawable(context.getResources(), bm);
-
-            drawable.setBounds(0, 0, bm.getWidth(), bm.getHeight());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return drawable;
-    }
-
-    @Override
-    protected void onPostExecute(Drawable result) {
-
-        final Drawable taskDrawable = result;
-
-        if (taskDrawable != null) {
-            taskTextView.setText(Html.fromHtml(taskHtmlString,
-                    new Html.ImageGetter() {
-
-                        @Override
-                        public Drawable getDrawable(String source) {
-                            return taskDrawable;
-                        }
-                    }, null));
+        Glide.with(htmlTextViewRemote).asBitmap()  .apply(new RequestOptions().override(40, 40))
+                .centerCrop() .load(source).into(new CustomTarget<Bitmap>() {
 
 
-            if(markDownImageClick!=null)
-            {
+            @Override
+            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                if (resource != null) {
 
-                      //  markDownImageClick.imageClicked(url);
+                    BitmapDrawable bitmapDrawable = new BitmapDrawable(context.getResources(),resource);
+                    drawable.addLevel(1, 1, bitmapDrawable);
+                    drawable.setBounds(0, 0, resource.getWidth()+20 , resource.getHeight()+20 );
+                    drawable.setLevel(1);
+                    htmlTextViewRemote.invalidate();
 
+                   // htmlTextViewRemote.setText(htmlStringRemote);
+
+                }
+            }
+
+            @Override
+            public void onLoadCleared(@Nullable Drawable placeholder) {
 
             }
-        }
 
+
+        });
+
+
+        return drawable;
 
     }
 
+    class HttpGetDrawableTask extends AsyncTask<String, Void, Drawable> {
+
+        private Context context;
+        private TextView taskTextView;
+        private String taskHtmlString;
+        MarkDownImageClick markDownImageClick;
+        String url;
+
+        HttpGetDrawableTask(Context context, TextView v, String s, MarkDownImageClick markDownImageClick) {
+            this.context = context;
+            taskTextView = v;
+            taskHtmlString = s;
+            this.markDownImageClick = markDownImageClick;
+        }
+
+        @Override
+        protected Drawable doInBackground(String... params) {
+            Drawable drawable = null;
+            URL sourceURL;
+            try {
+                sourceURL = new URL(params[0]);
+                url = params[0];
+                URLConnection urlConnection = sourceURL.openConnection();
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(
+                        inputStream);
+                Bitmap bm = BitmapFactory.decodeStream(bufferedInputStream);
+
+                // convert Bitmap to Drawable
+                drawable = new BitmapDrawable(context.getResources(), bm);
+                if (bm != null) {
+                    drawable.setBounds(0, 0, bm.getWidth(), bm.getHeight());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return drawable;
+        }
+
+        @Override
+        protected void onPostExecute(Drawable result) {
+
+            final Drawable taskDrawable = result;
+
+            if (taskDrawable != null) {
+                taskTextView.setText(Html.fromHtml(taskHtmlString,
+                        new Html.ImageGetter() {
+
+                            @Override
+                            public Drawable getDrawable(String source) {
+                                return taskDrawable;
+                            }
+                        }, null));
+
+
+                if (markDownImageClick != null) {
+
+                    //  markDownImageClick.imageClicked(url);
+
+
+                }
+            }
+
+
+        }
+
+    }
 }
+
