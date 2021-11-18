2b1a7b911c257a67027796b2d09a600fee442404
SudheerJa-Kore <sudheer.jampana@kore.com>
2021-11-18 13:56:20 +0530

    webhook_integrated

--- BotsSDK/korebotsdklib/src/main/java/kore/botssdk/net/SDKConfiguration.java
+++ BotsSDK/korebotsdklib/src/main/java/kore/botssdk/net/SDKConfiguration.java
@@ -133,7 +133,7 @@ public class SDKConfiguration {
 //        public static String bot_id = "st-05303785-9992-526c-a83c-be3252fd478e";//"st-2e4c9eaf-070c-5b86-8020-add76f37e3a2";//"st-05303785-9992-526c-a83c-be3252fd478e";//"st-caecd28f-64ed-5224-a612-7a3d3a870aed";//"st-cc32974e-c7a2-52d1-83bf-c3dc2b2a9db3";
         public static String tenant_id = "605da1dbb5f6f00badadb665";
         public static boolean enablePanel = false;
-        public static boolean isWebHook = true;
+        public static boolean isWebHook = false;
 
         public static String client_id = "cs-854a9d4d-9f34-5fdb-a5e3-6fa864ed77c8";//"cs-96c4747a-bb79-58b0-9dca-0dcf6c6148cf";//"cs-dc0f84ac-4751-5293-b254-6a0a382ab08c";//"cs-a269ad0a-45ec-5b41-9950-18571e42a6a4";//"cs-5649368e-68bb-555a-8803-935f223db585";
         public static String client_secret = "knoJVXBrxflCUczrwFYkTp2FK476oZneN7cb7Mqdxxc=";//"qc4c+FOpEo88m27BgECpcS/oC/CKDWa8u70ll0qr4MM=";//"MiFzNLLWTQZddj1HOmdM4iyePhQ+gED4jdUg88Ujh1Y=";//"kmZ7ck9wRxSVV2dNNwi2P3UZI3qacJgu7JL9AmZapS4=";//"AHSubkG09DRdcz9xlzxUXfrxyRx9V0Yhd+6SnXtjYe4=";
