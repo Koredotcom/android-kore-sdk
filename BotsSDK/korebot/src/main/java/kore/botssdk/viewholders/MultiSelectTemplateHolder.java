52e96babb7066d7934d3963bb622b77d526f23ef
Talloju Kishore Kumar <kishore.talloju@kore.com>
2024-08-06 14:34:41 +0530

    Replaced AutoExpandListView to RecyclerView in MultiSelecTemplateHolder

--- BotsSDK/korebot/src/main/java/kore/botssdk/viewholders/MultiSelectTemplateHolder.java
+++ BotsSDK/korebot/src/main/java/kore/botssdk/viewholders/MultiSelectTemplateHolder.java
@@ -5,20 +5,25 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import kore.botssdk.R;
-import kore.botssdk.adapter.MultiSelectButtonAdapter;
+import kore.botssdk.adapter.MultiSelectTemplateAdapter;
 import kore.botssdk.models.BaseBotMessage;
+import kore.botssdk.models.BotResponse;
 import kore.botssdk.models.MultiSelectBase;
 import kore.botssdk.models.PayloadInner;
 import kore.botssdk.utils.KaFontUtils;
-import kore.botssdk.view.AutoExpandListView;
 
 public class MultiSelectTemplateHolder extends BaseViewHolder {
-    private final AutoExpandListView autoExpandListView;
+    private final RecyclerView recyclerView;
     private final View multiSelectLayout;
+
     public static MultiSelectTemplateHolder getInstance(ViewGroup parent) {
         return new MultiSelectTemplateHolder(createView(R.layout.template_multi_select, parent));
     }
@@ -27,8 +32,9 @@ public class MultiSelectTemplateHolder extends BaseViewHolder {
         super(itemView, itemView.getContext());
         LinearLayoutCompat layoutBubble = itemView.findViewById(R.id.layoutBubble);
         initBubbleText(layoutBubble, false);
-        autoExpandListView = itemView.findViewById(R.id.multi_select_list);
-        autoExpandListView.setVerticalScrollBarEnabled(false);
+        recyclerView = itemView.findViewById(R.id.multi_select_list);
+        recyclerView.setVerticalScrollBarEnabled(false);
+        recyclerView.setLayoutManager(new LinearLayoutManager(itemView.getContext()));
         KaFontUtils.applyCustomFont(itemView.getContext(), itemView);
         multiSelectLayout = itemView.findViewById(R.id.multi_select_layout);
     }
@@ -37,17 +43,19 @@ public class MultiSelectTemplateHolder extends BaseViewHolder {
     public void bind(BaseBotMessage baseBotMessage) {
         PayloadInner payloadInner = getPayloadInner(baseBotMessage);
         if (payloadInner == null) return;
+        String msgId = ((BotResponse) baseBotMessage).getMessageId();
         multiSelectLayout.setAlpha(isLastItem() ? 1.0f : 0.5f);
         ArrayList<MultiSelectBase> items = new ArrayList<>();
         if (payloadInner.getMultiSelectModels() != null && payloadInner.getMultiSelectModels().size() > 0)
             items.addAll(payloadInner.getMultiSelectModels());
         if (payloadInner.getButtons() != null && payloadInner.getButtons().size() > 0)
             items.addAll(payloadInner.getButtons());
-        MultiSelectButtonAdapter multiSelectButtonAdapter = new MultiSelectButtonAdapter(itemView.getContext());
-        multiSelectButtonAdapter.setMultiSelectModels(items);
-        multiSelectButtonAdapter.setEnabled(isLastItem());
-        multiSelectButtonAdapter.setComposeFooterInterface(composeFooterInterface);
-        autoExpandListView.setAdapter(multiSelectButtonAdapter);
-        multiSelectButtonAdapter.notifyDataSetChanged();
+        MultiSelectTemplateAdapter multiSelectTemplateAdapter = new MultiSelectTemplateAdapter(itemView.getContext(), msgId, items, isLastItem());
+        Map<String, Object> state = ((BotResponse) baseBotMessage).getContentState();
+        if (state != null && state.containsKey(BotResponse.SELECTED_ITEM))
+            multiSelectTemplateAdapter.setCheckItems((List<MultiSelectBase>) state.get(BotResponse.SELECTED_ITEM));
+        multiSelectTemplateAdapter.setListener(contentStateListener);
+        multiSelectTemplateAdapter.setComposeFooterInterface(composeFooterInterface);
+        recyclerView.setAdapter(multiSelectTemplateAdapter);
     }
 }
