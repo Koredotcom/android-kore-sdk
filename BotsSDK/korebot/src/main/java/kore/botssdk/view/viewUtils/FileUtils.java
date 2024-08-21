a853d49ac8569120fadd9df45c53b58b63650c83
SudheerJa-Kore <sudheer.jampana@kore.com>
2024-08-21 13:40:12 +0530

    table_template_changes

--- BotsSDK/korebot/src/main/java/kore/botssdk/view/viewUtils/FileUtils.java
+++ BotsSDK/korebot/src/main/java/kore/botssdk/view/viewUtils/FileUtils.java
@@ -1,191 +1,13 @@
 package kore.botssdk.view.viewUtils;
 
-import android.graphics.Color;
-
 import java.util.ArrayList;
 
 import kore.botssdk.R;
 
-/**
- * Created by Ramachandra Pradeep on 13-Aug-18.
- */
-
 public class FileUtils {
-    public static final String EXT_DOTX = "dotx";
-    public static final String EXT_DOTM = "dotm";
-    public static final String EXT_DOCM = "docm";
-    public static final String EXT_DOT = "dot";
-    public static final String EXT_XLA = "xla";
-    public static final String EXT_XLT = "xlt";
-    public static final String EXT_XLM = "xlm";
-    public static final String EXT_XLL = "xll";
-    public static final String EXT_XLW = "xlw";
-    public static final String EXT_XLSB = "xlsb";
-    public static final String EXT_XLSM = "xlsm";
-    public static final String EXT_XLTX = "xltx";
-    public static final String EXT_XLTM = "xltm";
-    public static final String EXT_XLAM = "xlam";
-    public static final String EXT_POTM = "potm";
-    public static final String EXT_POTX = "potx";
-    public static final String EXT_SLDX = "sldx";
-    public static final String EXT_JPEG = "jpeg";
-    public static final String EXT_AUDIO_10 = "m4a";
-    public static final String EXT_AUDIO_9 = "amr";
-    public static final String EXT_AUDIO_MP3 = "mp3";
-    public static final String EXT_AUDIO_MPEG = "mpeg";
-    public static final String EXT_AUDIO_MID = "mid";
-    public static final String EXT_AUDIO_BASIC = "au";
-    public static final String EXT_AUDIO_SND = "snd";
-    public static final String EXT_AUDIO_WAV = "wav";
-    public static final String EXT_AUDIO_AAC = "aac";
-    public static final String EXT_AUDIO_RA = "ra";
-    public static final String EXT_AUDIO_RM = "rm";
-    public static final String EXT_AUDIO_WMA = "wma";
-    public static final String EXT_VIDEO_AVI = "avi";
-    public static final String EXT_VIDEO_MOV = "mov";
-    public static final String EXT_VIDEO_FLV = "flv";
-    public static final String EXT_VIDEO_WMV = "wmv";
-    public static final String EXT_VIDEO_VOB = "vob";
     public static final String EXT_VIDEO = "mp4";
-    public static final String EXT_VIDEO_3GP = "3gp";
-    public static final String EXT_VIDEO_3GP2 = "3gp2";
-    public static final String EXT_7ZIP = "7z";
-    public static final String EXT_ZIP = "zip";
-    public static final String EXT_RAR = "rar";
-    public static final String EXT_HTM = "htm";
-    public static final String EXT_HTML = "html";
-    public static final String EXT_AZW = "azw";
-
-    //3dimageFileFormats
-    public static final String EXT_3DM = "3dm";
-    public static final String EXT_3DS = "3ds";
-    public static final String EXT_MAX = "max";
-    public static final String EXT_OBJ = "obj";
-
-    //audiofileformats
-    public static final String EXT_AIF = "aif";
-    public static final String EXT_IFF = "iff";
-    public static final String EXT_M3U = "m3u";
-    public static final String EXT_M4A = "m4a";
-    public static final String EXT_MID = "mid";
-    public static final String EXT_MP3 = "mp3";
-    public static final String EXT_MPA = "mpa";
-    public static final String EXT_WAV = "wav";
-    public static final String EXT_WMA = "wma";
-
-    //datafileformats
-    public static final String EXT_CSV = "csv";
-    public static final String EXT_DAT = "dat";
-    public static final String EXT_GED = "ged";
-    public static final String EXT_KEY = "key";
-    public static final String EXT_KEYCHA = "keycha";
-    public static final String EXT_PPS = "pps";
-    public static final String EXT_PPT = "ppt";
-    public static final String EXT_PPTX = "pptx";
-    public static final String EXT_SDF = "sdf";
-    public static final String EXT_TAR = "tar";
-    public static final String EXT_VCF = "vcf";
-    public static final String EXT_XML = "xml";
-
-
-    //executableFileFormats
-    public static final String EXT_APK = "apk";
-    public static final String EXT_APP = "app";
-    public static final String EXT_BAT = "bat";
-    public static final String EXT_CGI = "cgi";
-    public static final String EXT_COM = "com";
-    public static final String EXT_EXE = "exe";
-    public static final String EXT_GADGET = "gadget";
-    public static final String EXT_JAR = "jar";
-    public static final String EXT_WSF = "wsf";
-
-
-    //otherFileFormats
-    public static final String EXT_ACCBD = "accbd";
-    public static final String EXT_DB = "db";
-    public static final String EXT_DBF = "dbf";
-    public static final String EXT_MDB = "mdb";
-    public static final String EXT_PDB = "pdb";
-    public static final String EXT_SQL = "sql";
-
-
-    //pagelayoutimageFileFormats
-    public static final String EXT_INDD = "indd";
-    public static final String EXT_PCT = "pct";
-    public static final String EXT_PDF = "pdf";
-
-
-    //rasterimageFileFormats
-    public static final String EXT_BMP = "bmp";
-    public static final String EXT_DDS = "dds";
-    public static final String EXT_GIF = "gif";
     public static final String EXT_JPG = "jpg";
     public static final String EXT_PNG = "png";
-    public static final String EXT_PSD = "psd";
-    public static final String EXT_PSPIMA = "pspima";
-    public static final String EXT_TGA = "tga";
-    public static final String EXT_THM = "thm";
-    public static final String EXT_TIF = "tif";
-    public static final String EXT_TIFF = "tiff";
-    public static final String EXT_YUV = "yuv";
-    public static final String EXT_ICO = "ico";
-
-
-    //spreadsheetFileFormats
-    public static final String EXT_XLR = "xlr";
-    public static final String EXT_XLS = "xls";
-    public static final String EXT_XLSX = "xlsx";
-    public static final String EXT_GSHEET = "gsheet";
-
-
-    //textFileFormats
-    public static final String EXT_DOC = "doc";
-    public static final String EXT_DOCX = "docx";
-    public static final String EXT_LOG = "log";
-    public static final String EXT_MSG = "msg";
-    public static final String EXT_ODT = "odt";
-    public static final String EXT_PAGES = "pages";
-    public static final String EXT_RTF = "rtf";
-    public static final String EXT_TEX = "tex";
-    public static final String EXT_TXT = "txt";
-    public static final String EXT_WPD = "wpd";
-    public static final String EXT_WPS = "wps";
-
-
-    //vectorimageFileFormats
-    public static final String EXT_AI = "ai";
-    public static final String EXT_EPS = "eps";
-    public static final String EXT_PS = "ps";
-    public static final String EXT_SKETCH = "sketch";
-    public static final String EXT_SVG = "svg";
-
-
-    //videofileformats
-    public static final String EXT_3G2 = "3g2";
-    public static final String EXT_3GP = "3gp";
-    public static final String EXT_ASF = "asf";
-    public static final String EXT_AVI = "avi";
-    public static final String EXT_FLV = "flv";
-    public static final String EXT_M4V = "m4v";
-    public static final String EXT_MOV = "mov";
-    public static final String EXT_MP4 = "mp4";
-    public static final String EXT_MPG = "mpg";
-    public static final String EXT_RM = "rm";
-    public static final String EXT_SRT = "srt";
-    public static final String EXT_SWF = "swf";
-    public static final String EXT_VOB = "vob";
-    public static final String EXT_WMV = "wmv";
-
-
-    public static final String COLOR_PDF_OR_PPTX = "#d0021b";
-    public static final String COLOR_XLS = "#5fad56";
-    public static final String COLOR_GIF = "#00c9fd";
-    public static final String COLOR_DOCX = "#2f7bf1";
-    public static final String COLOR_AUDIO = "#9e68bc";
-    public static final String COLOR_SKETCH = "#880010";
-    public static final String COLOR_TXT = "#8b93a0";
-    public static final String COLOR_WPS = "#2f7bf1";
-
     private static final ArrayList<String> videoTypes = new ArrayList<>();
     private static final ArrayList<String> audioTypes = new ArrayList<>();
     private static final ArrayList<String> docTypes = new ArrayList<>();
@@ -206,14 +28,12 @@ public class FileUtils {
 
     private static final String pdf = "pdf";
 
-    public static ArrayList<String> ImageTypes()
-    {
-        return  rasterImageTypes ;
+    public static ArrayList<String> ImageTypes() {
+        return rasterImageTypes;
     }
 
-    public static ArrayList<String> VideoTypes()
-    {
-        return  videoTypes ;
+    public static ArrayList<String> VideoTypes() {
+        return videoTypes;
     }
 
     static {
@@ -255,8 +75,8 @@ public class FileUtils {
         devTypes.add("vcxproj");
         devTypes.add("xcodeproj");
     }
-    private static void loadMsoffice()
-    {
+
+    private static void loadMsoffice() {
         msExcel.add("xls");
         msExcel.add("xlsx");
         msPowerpaint.add("ppt");
@@ -384,7 +204,7 @@ public class FileUtils {
         presentationTypes.add("ged");
         presentationTypes.add("key");
         presentationTypes.add("pps");
-         presentationTypes.add("gslide");
+        presentationTypes.add("gslide");
     }
 
     private static void loadThreeDImgTypes() {
@@ -417,73 +237,44 @@ public class FileUtils {
         vectorImgTypes.add("svg");
     }
 
-    public static int getColorForFileType(String fileType) {
-        if (fileType == null) return Color.parseColor(COLOR_DOCX);
-        if (fileType.equalsIgnoreCase(EXT_PDF) || fileType.equalsIgnoreCase(EXT_PPT) || fileType.equalsIgnoreCase(EXT_PPTX))
-            return Color.parseColor(COLOR_PDF_OR_PPTX);
-        else if (fileType.equalsIgnoreCase(EXT_TXT) || fileType.equalsIgnoreCase(EXT_GIF) || fileType.equalsIgnoreCase(EXT_ICO))
-            return Color.parseColor(COLOR_GIF);
-        else if (fileType.equalsIgnoreCase(EXT_PAGES) || fileType.equalsIgnoreCase(EXT_RTF) || fileType.equalsIgnoreCase(EXT_WPD))
-            return Color.parseColor(COLOR_WPS);
-        else if (fileType.equalsIgnoreCase(EXT_DOC) || fileType.equalsIgnoreCase(EXT_DOCX) || fileType.equalsIgnoreCase(EXT_JPG) || fileType.equalsIgnoreCase(EXT_JPEG)
-                || fileType.equalsIgnoreCase(EXT_MPG)) return Color.parseColor(COLOR_DOCX);
-        else if (fileType.equalsIgnoreCase(EXT_XLS) || fileType.equalsIgnoreCase(EXT_XLSX) || fileType.equalsIgnoreCase(EXT_GSHEET))
-            return Color.parseColor(COLOR_XLS);
-        else if (fileType.equalsIgnoreCase(EXT_MP3) || fileType.equalsIgnoreCase(EXT_WAV) || fileType.equalsIgnoreCase(EXT_MP4))
-            return Color.parseColor(COLOR_AUDIO);
-        else if (fileType.equalsIgnoreCase(EXT_SKETCH) || fileType.equalsIgnoreCase("crdownload"))
-            return Color.parseColor(COLOR_SKETCH);
-        else if (fileType.equalsIgnoreCase(EXT_TXT) || fileType.equalsIgnoreCase(EXT_HTML) || fileType.equalsIgnoreCase(EXT_ZIP) || fileType.equalsIgnoreCase(EXT_7ZIP)
-                || fileType.equalsIgnoreCase(EXT_JAR) || fileType.equalsIgnoreCase("js") || fileType.equalsIgnoreCase("java")
-                || fileType.equalsIgnoreCase(EXT_SQL)) return Color.parseColor(COLOR_TXT);
-        else return Color.parseColor(COLOR_DOCX);
-    }
-
-    public static int getDrawableByExt(String ext){
-        if(videoTypes.contains(ext)){
+    public static int getDrawableByExt(String ext) {
+        if (videoTypes.contains(ext)) {
             return R.drawable.ic_video;
-        }
-        else if(msDoc.contains(ext))
-        {
+        } else if (msDoc.contains(ext)) {
             return R.drawable.ic_document;
-        }
-        else if(msPowerpaint.contains(ext))
-        {
+        } else if (msPowerpaint.contains(ext)) {
             return R.drawable.ic_slides;
-        }
-        else if(msExcel.contains(ext))
-        {
+        } else if (msExcel.contains(ext)) {
             return R.drawable.ic_sheet;
-        }
-        else if(audioTypes.contains(ext)){
+        } else if (audioTypes.contains(ext)) {
             return R.drawable.ic_music;
-        }else if(spreadTypes.contains(ext)){
+        } else if (spreadTypes.contains(ext)) {
             return R.drawable.ic_sheet_old;
-        }else if(docTypes.contains(ext)){
+        } else if (docTypes.contains(ext)) {
             return R.drawable.ic_document_old;
-        }else if(threeDImgTypes.contains(ext)){
+        } else if (threeDImgTypes.contains(ext)) {
             return R.drawable.ic_3d_object;
-        }else if(rasterImageTypes.contains(ext)){
+        } else if (rasterImageTypes.contains(ext)) {
             return R.drawable.ic_raster_image;
-        }else if(vectorImgTypes.contains(ext)){
+        } else if (vectorImgTypes.contains(ext)) {
             return R.drawable.ic_vector_type;
-        }else if(dbTypes.contains(ext)){
+        } else if (dbTypes.contains(ext)) {
             return R.drawable.ic_database_type;
-        }else if(devTypes.contains(ext)){
+        } else if (devTypes.contains(ext)) {
             return R.drawable.ic_developer;
-        }else if(exeTypes.contains(ext)){
+        } else if (exeTypes.contains(ext)) {
             return R.drawable.ic_executable;
-        }else if(systemTypes.contains(ext)){
+        } else if (systemTypes.contains(ext)) {
             return R.drawable.ic_settings;
-        }else if(compressedTypes.contains(ext)){
+        } else if (compressedTypes.contains(ext)) {
             return R.drawable.ic_zip;
-        }else if(presentationTypes.contains(ext)){
+        } else if (presentationTypes.contains(ext)) {
             return R.drawable.ic_slides_old;
-        }else if(fontTypes.contains(ext)){
+        } else if (fontTypes.contains(ext)) {
             return R.drawable.ic_font;
-        }else if(pdf.equals(ext)){
+        } else if (pdf.equals(ext)) {
             return R.drawable.ic_pdf;
-        }else{
+        } else {
             return R.drawable.ic_file_general;
         }
     }
--- BotsSDK/korebot/src/main/java/kore/botssdk/viewholders/TableTemplateHolder.java
+++ BotsSDK/korebot/src/main/java/kore/botssdk/viewholders/TableTemplateHolder.java
@@ -1,7 +1,14 @@
 package kore.botssdk.viewholders;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.LinearLayoutCompat;
@@ -20,16 +27,19 @@ import kore.botssdk.adapter.TableTemplateHeaderAdapter;
 import kore.botssdk.itemdecoration.VerticalSpaceItemDecoration;
 import kore.botssdk.models.BaseBotMessage;
 import kore.botssdk.models.PayloadInner;
+import kore.botssdk.view.BotTableView;
 import kore.botssdk.view.tableview.model.MiniTableModel;
 
 public class TableTemplateHolder extends BaseViewHolder {
     private final RecyclerView rvTableView;
     private final RecyclerView rvTableViewHeader;
+    private TextView botTableShowMoreButton;
 
     public TableTemplateHolder(@NonNull View itemView) {
         super(itemView, itemView.getContext());
         rvTableView = itemView.findViewById(R.id.rvTableView);
         rvTableViewHeader = itemView.findViewById(R.id.rvTableViewHeader);
+        botTableShowMoreButton = itemView.findViewById(R.id.botTableShowMoreButton);
         rvTableView.setLayoutManager(new LinearLayoutManager(itemView.getContext(), LinearLayoutManager.VERTICAL, false));
         rvTableView.addItemDecoration(new VerticalSpaceItemDecoration(LinearLayoutManager.VERTICAL));
         LinearLayoutCompat layoutBubble = itemView.findViewById(R.id.layoutBubble);
@@ -52,11 +62,67 @@ public class TableTemplateHolder extends BaseViewHolder {
         int size = ((ArrayList<?>) payloadInner.getElements()).size();
         for (int j = 0; j < size; j++) {
             MiniTableModel model = new MiniTableModel();
-            model.setElements((List<Object>) (((LinkedTreeMap<?,?>)((ArrayList<?>) payloadInner.getElements()).get(j))).get("Values"));
+            model.setElements((List<Object>) (((LinkedTreeMap<?, ?>) ((ArrayList<?>) payloadInner.getElements()).get(j))).get("Values"));
             lists.add(model);
         }
 
         rvTableView.setLayoutManager(new LinearLayoutManager(itemView.getContext(), LinearLayoutManager.VERTICAL, false));
         rvTableView.setAdapter(new TableTemplateAdapter(itemView.getContext(), payloadInner.getColumns(), lists, isLastItem()));
+
+        botTableShowMoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTableViewDialog(itemView.getContext(), payloadInner);
+            }
+        });
+    }
+
+    private void showTableViewDialog(Context context, PayloadInner payloadInner) {
+        Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.tableview_dialog);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.gravity = Gravity.CENTER;
+
+        dialog.getWindow().setAttributes(lp);
+
+        LinearLayout llTableView = dialog.findViewById(R.id.llTableView);
+        RecyclerView rvTableView = dialog.findViewById(R.id.rvTableView);
+        ImageView ivDialogClose = dialog.findViewById(R.id.ivDialogClose);
+
+//        BotTableView tableView = new BotTableView(context);
+//        tableView.setComposeFooterInterface(composeFooterInterface);
+//        tableView.setInvokeGenericWebViewInterface(invokeGenericWebViewInterface);
+//        tableView.setDialogReference(dialog);
+//        tableView.setData(payloadInner);
+//        llTableView.addView(tableView);
+
+        rvTableView.setAdapter(new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return TableTemplateHolder.getInstance(parent);
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 3;
+            }
+        });
+
+        ivDialogClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
