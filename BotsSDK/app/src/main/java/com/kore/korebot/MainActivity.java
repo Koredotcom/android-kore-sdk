0414d485c4c498c6bc9d9853790532cd86732f60
SudheerJa-Kore <sudheer.jampana@kore.com>
2024-09-16 13:31:04 +0530

    Updated build.gradle files and added maven-publish

--- BotsSDK/app/src/main/java/com/kore/korebot/MainActivity.java
+++ BotsSDK/app/src/main/java/com/kore/korebot/MainActivity.java
@@ -5,8 +5,10 @@ import static android.Manifest.permission.POST_NOTIFICATIONS;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,8 +20,11 @@ import androidx.core.content.ContextCompat;
 
 import com.kore.korebot.customtemplates.LinkTemplateHolder;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.TimeZone;
 
 import kore.botssdk.activity.BotChatActivity;
@@ -42,28 +47,28 @@ public class MainActivity extends AppCompatActivity {
         String jwtToken = "";
 
         //Set clientId, If jwtToken is empty this value is mandatory
-        String clientId = "PLEASE_ENTER_BOT_CLIENT_ID";//getConfigValue("clientId")
+        String clientId = getConfigValue("clientId"); // "PLEASE_ENTER_BOT_CLIENT_ID";//
 
         //Set clientSecret, If jwtToken is empty this value is mandatory
-        String clientSecret = "PLEASE_ENTER_BOT_CLIENT_SECRET";//getConfigValue("clientSecret")
+        String clientSecret = getConfigValue("clientSecret"); //"PLEASE_ENTER_BOT_CLIENT_SECRET";//
 
         //Set botId, This value is mandatory
-        String botId = "PLEASE_ENTER_BOT_ID";//getConfigValue("botId")
+        String botId = getConfigValue("botId"); //"PLEASE_ENTER_BOT_ID";//
 
         //Set identity, This value is mandatory
-        String identity = "PLEASE_ENTER_IDENTITY"; //PLEASE_ENTER_IDENTITY getConfigValue("identity")
+        String identity = getConfigValue("identity"); //"PLEASE_ENTER_IDENTITY"; //PLEASE_ENTER_IDENTITY
 
         //Set botName, This value is mandatory
-        String botName = "PLEASE_ENTER_BOT_NAME";// getConfigValue("botName")
+        String botName = getConfigValue("botName"); //"PLEASE_ENTER_BOT_NAME";//
 
         //Set serverUrl, This value is mandatory
-        String serverUrl = "PLEASE_ENTER_SERVER_URL";// getConfigValue("serverUrl")
+        String serverUrl = getConfigValue("serverUrl"); //"PLEASE_ENTER_SERVER_URL";//
 
         //Set brandingUrl, This value is mandatory
-        String brandingUrl = "PLEASE_ENTER_BRANDING_SERVER_URL";// getConfigValue("brandingUrl")
+        String brandingUrl = getConfigValue("brandingUrl");// "PLEASE_ENTER_BRANDING_SERVER_URL";//
 
         //Set jwtServerUrl, This value is mandatory
-        String jwtServerUrl = "PLEASE_ENTER_JWT_SERVER_URL";// getConfigValue("jwtServerUrl")
+        String jwtServerUrl = getConfigValue("jwtServerUrl"); //"PLEASE_ENTER_JWT_SERVER_URL";//
 
         //Set Server url
         SDKConfig.setServerUrl(serverUrl);
@@ -163,18 +168,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    public String getConfigValue(String name) {
-//        try {
-//            InputStream rawResource = getResources().openRawResource(R.raw.config);
-//            Properties properties = new Properties();
-//            properties.load(rawResource);
-//            return properties.getProperty(name);
-//        } catch (Resources.NotFoundException e) {
-//            Log.e(MainActivity.class.getSimpleName(), "Unable to find the config file: " + e.getMessage());
-//        } catch (IOException e) {
-//            Log.e(MainActivity.class.getSimpleName(), "Failed to open config file.");
-//        }
-//
-//        return null;
-//    }
+    public String getConfigValue(String name) {
+        try {
+            InputStream rawResource = getResources().openRawResource(R.raw.config);
+            Properties properties = new Properties();
+            properties.load(rawResource);
+            return properties.getProperty(name);
+        } catch (Resources.NotFoundException e) {
+            Log.e(MainActivity.class.getSimpleName(), "Unable to find the config file: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(MainActivity.class.getSimpleName(), "Failed to open config file.");
+        }
+
+        return null;
+    }
 }
